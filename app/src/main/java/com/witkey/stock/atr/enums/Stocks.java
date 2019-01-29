ac0531f56a71f14ca1b44f76db73119b29b3f51f
fanshuaijun <fanshuaijun@lianjia.com>
2019-01-29 17:59:48 +0800

    atr

--- app/src/main/java/com/witkey/stock/atr/enums/Stocks.java
+++ app/src/main/java/com/witkey/stock/atr/enums/Stocks.java
@@ -5,12 +5,15 @@ import com.witkey.stock.atr.R;
 //@Getter
 //@AllArgsConstructor
 public enum Stocks {
+    MAOTAI("贵州茅台", "600519.SS", R.drawable.sh600519),
+    YANGHE("洋河股份", "002304.SZ", R.drawable.sz002304),
+    GUJINGGONG("古井贡酒", "000596.SZ", R.drawable.sz000596),
+    FENZHONG("分众传媒", "002027.SZ", R.drawable.sz002027),
     CHONGBAI("重庆百货", "600729.SS", R.drawable.sh600729),
     TIANTAN("天坛生物", "600161.SS", R.drawable.sh600161),
     HUALAN("华兰生物", "002007.SZ", R.drawable.sz002007),
     HAIKANG("海康威视", "002415.SZ", R.drawable.sz002415),
-    SHENNAN("深南电路", "002916.SZ", R.drawable.sz002916),
-    FENZHONG("分众传媒", "002027.SZ", R.drawable.sz002027);
+    SHENNAN("深南电路", "002916.SZ", R.drawable.sz002916);
     private String name;
     private String no;
     private Integer imageId;
